77e34e7b97ce96b28d4539721c58f979c849f682
Paul Vecchio <vecchp@gmail.com>
2017-10-03 22:21:08 -0700

    working through how we want to scale data

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 from numpy.polynomial.chebyshev import chebval
 import numpy as np
+import pandas as pd
+from sklearn import preprocessing
 
-def normalize_data():
 
-    pass
-
+def normalize_data(lower, upper):
+    data = np.concatenate((lower, upper), axis=0).reshape(-1, 1)
+    scale = preprocessing.MinMaxScaler(feature_range=(-1, 1)).fit(data)
+    print(scale.transform(data))
 
 
 def generate_cheb(x, num_coeffs):
     return chebval(x, np.eye(num_coeffs))
 
+
+def absolute_error(data, error=.01):
+    return data - error, data + error
+
+
 def main():
-    data_set = 'Kirby2.dat'
-    # data = pd.read_csv(data_set, delim_whitespace=True).as_matrix()
+    filename = 'Kirby2.dat'
+    data = pd.read_csv(filename, delim_whitespace=True).as_matrix()
 
+    lower, upper = absolute_error(data[:, -1])
 
-    print(generate_cheb([1,2,3,4], 4))
+    normalize_data(lower, upper)
+
+    # print(generate_cheb([1, 2, 3, 4], 4))
 
 
 if __name__ == '__main__':
     main()
 
+# Normalize inputs
+# Normalize data
 
+# simple just scale between max and mix w.r.t error margin
+#   min = mindata-lower_bound
+#   max = maxdata+upper_bound
